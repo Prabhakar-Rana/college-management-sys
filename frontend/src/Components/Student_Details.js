79b07f286640380bcef466e015ed0c38b658f2a5
Prabhakar-Rana <94912014+Prabhakar-Rana@users.noreply.github.com>
2024-10-08 00:39:34 +0530

    Update Student_Details.js

--- frontend/src/Components/Student_Details.js
+++ frontend/src/Components/Student_Details.js
@@ -12,7 +12,8 @@ export const Student_Details = () => {
   useEffect(() => {
     // Fetch data from API when the component mounts
     axios
-      .get(`http://localhost:5000/api/students/${id}`) // Replace with your API URL
+      .get(`college-management-sys-backend.vercel.app
+/api/students/${id}`) // Replace with your API URL
       .then((response) => {
         console.log("result==",response.data);
         setData(response.data);
