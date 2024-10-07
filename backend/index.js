33d79ab91e2ab5b44723e0341337f44fcf3172db
Prabhakar-Rana <94912014+Prabhakar-Rana@users.noreply.github.com>
2024-10-08 02:09:02 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ app.use('/api/students', studentRoutes);
 app.use('/api/courses', courseRoutes);
 app.use('/api/faculty', facultyRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
