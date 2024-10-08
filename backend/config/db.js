9ed4ae2fe9c254792c9f187cb2698802067533d8
Prabhakar-Rana <94912014+Prabhakar-Rana@users.noreply.github.com>
2024-10-08 12:30:33 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/collegeDB');  // Options removed
+    await mongoose.connect('mongodb+srv://ranaabckqr:RanaPrabha%40123@collagecluster.e8ti4.mongodb.net/collegeDB');  // Options removed
     console.log('MongoDB connected');
   } catch (err) {
     console.error(err.message);
